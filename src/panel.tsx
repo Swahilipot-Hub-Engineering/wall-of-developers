520b4578bbb4dab7e3af2f7992931e129438a527
benjamin <titusben64@gmail.com>
2025-05-21 09:07:52 +0300

    my commit changes

---
+++ src/panel.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+// Define the InfoPanel component
+const InfoPanel: React.FC = () => {
+  return (
+    <div>
+      InfoPanel content goes here.
+    </div>
+  );
+};
+
+export default InfoPanel;
